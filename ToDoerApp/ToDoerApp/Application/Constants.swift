18eff33f49fb7aa0fd43d7510067f798289fa9b0
TonyJem <a.jemanov@gmail.com>
2022-04-02 13:19:38 +0300

    Create Constants struct in separate File

---
+++ ToDoerApp/ToDoerApp/Application/Constants.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+struct Constants {
+    enum Layout {
+        static let screenWidth = UIScreen.main.bounds.width
+        static let screenHeight = UIScreen.main.bounds.height
+        static let minimumLineSpacing: CGFloat = 0.0
+    }
+}
