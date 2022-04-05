4088c0fec86a9bfc826236c7168d0387fd4658c4
TonyJem <a.jemanov@gmail.com>
2022-04-05 18:56:59 +0300

    Update TopTabPanelView to be stackView and add hardcoded Tabs into it

--- ToDoerApp/ToDoerApp/Views/Header/TopTabPanelView.swift
+++ ToDoerApp/ToDoerApp/Views/Header/TopTabPanelView.swift
@@ -1,9 +1,37 @@
 import UIKit
 
-class TopTabPanelView: UIView {
+class TopTabPanelView: UIStackView {
     
-    let tabView: TopTab = {
+    let tabView0: TopTab = {
         let tab = TopTab(frame: .zero, tab: Tab(title: "Day", defaultColor: .tabDayBackround), index: 1)
+        
+        tab.translatesAutoresizingMaskIntoConstraints = false
+        return tab
+    }()
+    
+    let tabView1: TopTab = {
+        let tab = TopTab(frame: .zero, tab: Tab(title: "Week", defaultColor: .tabWeekBackround), index: 1)
+        
+        tab.translatesAutoresizingMaskIntoConstraints = false
+        return tab
+    }()
+    
+    let tabView2: TopTab = {
+        let tab = TopTab(frame: .zero, tab: Tab(title: "Month", defaultColor: .tabMonthBackround), index: 1)
+        
+        tab.translatesAutoresizingMaskIntoConstraints = false
+        return tab
+    }()
+    
+    let tabView3: TopTab = {
+        let tab = TopTab(frame: .zero, tab: Tab(title: "Tasks", defaultColor: .tabTasksBackround), index: 1)
+        
+        tab.translatesAutoresizingMaskIntoConstraints = false
+        return tab
+    }()
+    
+    let tabView4: TopTab = {
+        let tab = TopTab(frame: .zero, tab: Tab(title: "Notes", defaultColor: .tabNotesBackround), index: 1)
         
         tab.translatesAutoresizingMaskIntoConstraints = false
         return tab
@@ -13,20 +41,26 @@ class TopTabPanelView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .green
+        spacing = -7
+        axis  = NSLayoutConstraint.Axis.horizontal
+        distribution = UIStackView.Distribution.fillEqually
         translatesAutoresizingMaskIntoConstraints = false
         
         setupTabs()
         setConstraints()
     }
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Private Methods
     private func setupTabs() {
-        addSubview(tabView)
+        addArrangedSubview(tabView0)
+        addArrangedSubview(tabView1)
+        addArrangedSubview(tabView2)
+        addArrangedSubview(tabView3)
+        addArrangedSubview(tabView4)
     }
 }
 
@@ -35,11 +69,23 @@ extension TopTabPanelView {
     private func setConstraints() {
         
         NSLayoutConstraint.activate([
-            tabView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 200),
-            tabView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            tabView.widthAnchor.constraint(equalToConstant: 100),
-            tabView.heightAnchor.constraint(equalToConstant: Constants.TopTabPanel.height)
+            tabView0.heightAnchor.constraint(equalToConstant: Constants.TopTabPanel.height)
         ])
         
+        NSLayoutConstraint.activate([
+            tabView1.heightAnchor.constraint(equalToConstant: Constants.TopTabPanel.height)
+        ])
+        
+        NSLayoutConstraint.activate([
+            tabView2.heightAnchor.constraint(equalToConstant: Constants.TopTabPanel.height)
+        ])
+        
+        NSLayoutConstraint.activate([
+            tabView3.heightAnchor.constraint(equalToConstant: Constants.TopTabPanel.height)
+        ])
+        
+        NSLayoutConstraint.activate([
+            tabView4.heightAnchor.constraint(equalToConstant: Constants.TopTabPanel.height)
+        ])
     }
 }
