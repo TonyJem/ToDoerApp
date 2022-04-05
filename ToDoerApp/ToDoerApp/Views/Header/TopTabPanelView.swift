d1594c9f4f95b4b6cc29c66dd3c7ab4fdcfc2e37
TonyJem <a.jemanov@gmail.com>
2022-04-05 15:49:40 +0300

    Add tabView to TopTabPanelView

--- ToDoerApp/ToDoerApp/Views/Header/TopTabPanelView.swift
+++ ToDoerApp/ToDoerApp/Views/Header/TopTabPanelView.swift
@@ -2,16 +2,44 @@ import UIKit
 
 class TopTabPanelView: UIView {
     
+    let tabView: TopTab = {
+        let tab = TopTab(frame: .zero, tab: Tab(title: "Day", defaultColor: .tabDayBackround), index: 1)
+        
+        tab.translatesAutoresizingMaskIntoConstraints = false
+        return tab
+    }()
+    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .green
         translatesAutoresizingMaskIntoConstraints = false
+        
+        setupTabs()
+        setConstraints()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
+    // MARK: - Private Methods
+    private func setupTabs() {
+        addSubview(tabView)
+    }
+}
+
+// MARK: - SetConstraints
+extension TopTabPanelView {
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            tabView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tabView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tabView.widthAnchor.constraint(equalToConstant: 100),
+            tabView.heightAnchor.constraint(equalToConstant: Constants.TopTabPanel.height)
+        ])
+        
+    }
 }
