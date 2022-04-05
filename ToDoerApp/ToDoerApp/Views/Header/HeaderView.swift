89a3e81421712553cf5ea17c1288fc8c629258cd
TonyJem <a.jemanov@gmail.com>
2022-04-05 15:17:50 +0300

    Add tabPanelView to HeaderView

--- ToDoerApp/ToDoerApp/Views/Header/HeaderView.swift
+++ ToDoerApp/ToDoerApp/Views/Header/HeaderView.swift
@@ -2,15 +2,42 @@ import UIKit
 
 class HeaderView: UIView {
     
+    // MARK: - Views
+    private let tabPanelView = TabPanelView()
+    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         backgroundColor = .gray
         translatesAutoresizingMaskIntoConstraints = false
+        
+        setupViews()
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func setupViews() {
+        addSubview(tabPanelView)
+    }
+}
+
+// MARK: - SetConstraints
+extension HeaderView {
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            tabPanelView.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                                  constant: Constants.BottomTabPanel.horizontalInset),
+            tabPanelView.topAnchor.constraint(equalTo: topAnchor),
+            tabPanelView.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                   constant: -1 * Constants.BottomTabPanel.horizontalInset),
+            tabPanelView.heightAnchor.constraint(equalToConstant: Constants.BottomTabPanel.height)
+        ])
+        
     }
 }
